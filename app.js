5ade01fcc97a515f4ba979427f06b057507a676b
Daniel <danleavitt0@gmail.com>
2015-02-03 22:26:51 -0800

    go live

--- app.js
+++ app.js
@@ -69,7 +69,7 @@
     });
 
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '/public', 'index.html'));
+        res.sendfile(path.join('public/index.html'));
     });
 
 
